5732e26da5f69c56549ac00df57b7b53797d6b16
Joe Huelsman <kcgooner91@gmail.com>
2020-04-30 09:25:25 -0500

    added app.js and index.js code

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {BrowserRouter as Router, Route } from "react-router-dom";
+import Portfolio from "./pages/about/";
+import Contact from "./pages/contact";
+import Projects from "./pages/projects";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Wrapper from "./components/wrapper";
+
+function App() {
+  document.title = "About Me";
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Wrapper>
+          <Route exact path ="/" component={About} />
+          <Route exact path ="/contact" component={Contact} />
+          <Route exact path ="/projects" component={Projects} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App;
