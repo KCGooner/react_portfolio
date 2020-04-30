ab3cd7039f2ef9a0be09d073c10d90c44ad02a51
Joe Huelsman <kcgooner91@gmail.com>
2020-04-30 15:56:50 -0500

    herkoku deploy error

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -16,7 +16,7 @@ function About() {
                         <img className="headshot img-fluid" src="./me.jpg"/>
                     </div>
                     <div className="col-12 col-md-8 pt-2 pt-md-0">
-                        <p>Hello, my name is Joe Huelsman and I am twenty eight years old. I am from Gardner, Kansas where I grew up with a small family just my parents, my younger brother, and myself. In high school I had a passion for all things computers and ended up taking some coding classes my sophomore thru senior year of high school. I have always had an interest in how the internet and websites work and have recently decided to go back to school and I am now enrolled in the KU Coding Boot Camp program. I will be sharing some of the work that I have done over the course of the class. I hope that you will be able to see my skills grow and develop and I aim to become a proficient and hardworking developer.</p>
+                        <p>Hello, my name is Joe Huelsman and I am twenty eight years old. I am from Gardner, Kansas where I grew up with a small family just my parents, my younger brother, and myself. In high school I had a passion for all things computers and ended up taking some coding classes my sophomore thru senior year of high school. I have always had an interest in how the internet and websites work and have recently decided to go back to school and I am now enrolled in the University of Kansas Coding Boot Camp program. I will be sharing some of the work that I have done over the course of the class. I hope that you will be able to see my skills grow and develop and I aim to become a proficient and hardworking developer.</p>
                     </div>
                 </div>
             </div>
