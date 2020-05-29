5eeb75d2c8a2e75d39167c7c27a90ef88c8945ad
Joe Huelsman <kcgooner91@gmail.com>
2020-05-29 14:25:02 -0500

    edited some of my about me page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -14,31 +14,33 @@ function About() {
                 </div>
                 <div className="row mt-2">
                     <div className="col-12 col-md-4">
-                        <img className="headshot img-fluid" src= {Profile}/>
+                        <img className="headshot img-fluid" src={Profile} />
                     </div>
                     <div className="col-12 col-md-8 pt-2 pt-md-0">
-                        <p>Hello, my name is Joe Huelsman and I am twenty eight years old. I am from Gardner, Kansas where I grew up with a small family just my parents, my younger brother, and myself. In high school I had a passion for all things computers and ended up taking some coding classes my sophomore thru senior year of high school. I have always had an interest in how the internet and websites work and have recently decided to go back to school and I am now enrolled in the University of Kansas Coding Boot Camp program. I will be sharing some of the work that I have done over the course of the class. I hope that you will be able to see my skills grow and develop and I aim to become a proficient and hardworking developer.</p>
-                        <h3>Skills: </h3>
-                        <ul>
-                            <li>
-                                CSS
+                        <p>Hello, my name is Joe Huelsman I am from Gardner, Kansas where I grew up with a small family just my parents, my younger brother, and myself. In high school I had a passion for all things computers and ended up taking some coding classes my sophomore thru senior year of high school. I have always had an interest in how the internet and websites work and I have completed the KU Coding Bootcamp program with a Certificate of Completion. I will be sharing some of the work that I have done over the course of the class. I hope that you will be able to see my skills grow and develop and I aim to become a proficient and hardworking developer.</p>
+                        <div>
+                            <h3>Skills: </h3>
+                            <ul>
+                                <li>
+                                    CSS
                             </li>
-                            <li>
-                                HTML
+                                <li>
+                                    HTML
                             </li>
-                            <li>
-                                JavaScript
+                                <li>
+                                    JavaScript
                             </li>
-                            <li>
-                                Node.js
+                                <li>
+                                    Node.js
                             </li>
-                            <li>
-                                React
+                                <li>
+                                    React
                             </li>
-                            <li>
-                                MongoDB
+                                <li>
+                                    MongoDB
                             </li>
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
