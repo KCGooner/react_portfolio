eabf15efe167d0764fc7b3807a4424297ce0aa69
Joe Huelsman <kcgooner91@gmail.com>
2020-07-16 12:16:48 -0500

    made headshot responsive

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -14,7 +14,7 @@ function About() {
                 </div>
                 <div className="row mt-2">
                     <div className="col-12 col-md-4">
-                        <img className="headshot img-fluid" src={Profile} />
+                        <img className="headshot img-responsive" src={Profile} />
                     </div>
                     <div className="col-12 col-md-8 pt-2 pt-md-0">
                         <p>Hello, my name is Joe Huelsman I am from Gardner, Kansas where I grew up with a small family just my parents, my younger brother, and myself. In high school I had a passion for all things computers and ended up taking some coding classes my sophomore thru senior year of high school. I have always had an interest in how the internet and websites work and I have completed the KU Coding Bootcamp program with a Certificate of Completion. I will be sharing some of the work that I have done over the course of the class. I hope that you will be able to see my skills grow and develop and I aim to become a proficient and hardworking developer.</p>
